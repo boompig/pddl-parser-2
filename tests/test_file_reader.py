0f244082df1b43015365f3fd3d96ba283ef21598
Daniel Kats <boompigdev@gmail.com>
2019-10-02 19:47:45 -0700

    add tests for file reader
    tests pass for almost all pond-benchmarks
    I haven't tried them with all files in replanner because there are over
    1000 and it's a bit slow to parse.

--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -53,4 +53,22 @@ def test_read_all_files_clg():
         )
         assert problem is not None
         n += 1
-    assert n == 200
+    assert n == 73
+
+
+def test_read_all_files_contff():
+    d = "pond-benchmarks/contff-tests"
+    n = 0
+    for p_fname, d_fname in read_all_files(d):
+        assert d_fname is not None
+        # make sure valid paths are returned
+        assert os.path.exists(p_fname)
+        assert os.path.exists(d_fname)
+        # resultant domain and problem files should go together to create a problem
+        problem = Problem(
+            d_fname,
+            p_fname
+        )
+        assert problem is not None
+        n += 1
+    assert n == 69
